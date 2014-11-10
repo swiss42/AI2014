3caff98a396bb9a8dbe1e56ad82e8e9ea4e415e5
blake <blakebowie@gmail.com>
2014-11-09 23:18:40 -0600

    setting up the initial stuff

--- Hw4/my_text_interface.py
+++ Hw4/my_text_interface.py
@@ -62,6 +62,7 @@ class MyDialog:
             self.log_error("Already parsed plan: {0}, cannot parse new plan.".format(self.parsed_plan))
         
     def semantic_parser(self, plan):
+            #move disk1 from pole1 to pole2 and move disk2 from pole1 to pole3
             #Write code for simple semantic grammar here
             #Actions should be returned in the following format:
             #1. Mov Object Source Destination
@@ -75,34 +76,43 @@ class MyDialog:
         words = string.split(plan) # A list of the words in the user command
         result = plan # In case we cannot parse the plan propperly, return it unmodified
 
+        and_index = words.index("and") #and location
 
-		#The verb phrase should come first, so look at the first word
-		#If the verb phase is present go ahead and parse the noun phrases
+        result = self.get_verb_phrase(words) + self.get_noun_phrases(words)
+
+        if and_index >= 0:
+            second_command = words[and_index + 1:]
+            result += self.get_verb_phrase(second_command) + self.get_noun_phrases(words[and_index + 1:])
+
+        return result
+
+    def get_verb_phrase(self, words):
+        #The verb phrase should come first, so look at the first word
+        #If the verb phase is present go ahead and parse the noun phrase
         self.log_info("Expanding sentence 'S' to VP1 NP1 NP3 NP4 | VP2 NP1 NP3 | VP3 NP1 NP4,")
         self.log_info("the identifying initial verb phrase...")
         if words[0] == "mov" or words[0] == "move":
             self.log_info("Case VP1 NP1 NP3 NP4 (\"Move\") found")
             self.log_info("Identifying noun phrases...")
-            result = "Mov " + self.get_noun_phrases(plan)
+            result = "Mov " 
         elif words[0] == "pick":
             self.log_info("Case VP2 NP1 NP3 (\"Move\") found.")
             self.log_info("Identifying noun phrases...")            
-            result = "Pick " + self.get_noun_phrases(plan)
+            result = "Pick "
         elif words[0] == "put":
             self.log_info("Case VP3 NP1 NP4 (\"Move\") found.")
             self.log_info("Identifying noun phrases...")            
-            result = "Put " + self.get_noun_phrases(plan)
+            result = "Put "
         else:
             self.log_error(words, "Instructions must begin with Move, Pick or Put.")
         return result
     
-    def get_noun_phrases(self, plan):
+    def get_noun_phrases(self, words):
         """
         Helper method. Given a string representing a plan,
         finds the noun phrases by removing every word other than "disk*" or "pole*.
 		Does not check whether these are in the correct order."
         """
-        words = string.split(plan)
         result = ""
         #asssume that the noun phrases are in a fixed order
         #then just look for the keywords "disk" and "pole", ignoring everything elsee
